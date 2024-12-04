90ce0358bc907ee2cbee050ee153fb683e1d4c5d
Seyibamzee <okalabi@cps.edu>
2024-12-04 15:51:58 +0000

    gg

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let leftRight = document.querySelector('.leftRight');
 let text = document.querySelector('p');
 let room = document.querySelector('.room');
 room.style.display="none";
-let burp = document.querySelector('.burp');
-burp.style.display="none";
 let cheese = document.querySelector('.cheese');
 cheese.style.display="none";
+let burp= document.querySelector('.burp');
+burp.style.display="none"
 let eatCheese= document.querySelector('.eatCheese');
 eatCheese.style.display="none";
+let grabs = document.querySelector('.grabs');
+grabs.style.display="none";
 // left left left left left left left left left left left left 
 
 left.addEventListener('click', function()  {
@@ -19,18 +21,21 @@ left.addEventListener('click', function()  {
 eatCheese.style.display="block";
     room.style.display="block";
     leftRight.style.display="none";
-
-   text.innerHTML="";
+   text.innerHTML="As you tried to open the closet a hand came out and grabbbed the door ( Double click to run out or find chesse to talk to it)";
+   grabs.style.display="none";
 });
 
 // left choice 1 left choice 1 left choice 1 left choice 1 left choice 1 left choice 1 
 room.addEventListener('dblclick' , function() {
- text.innerHTML="that was good";
+ text.innerHTML="You ran into your parents room woke them up and told them everything and they checked but found nothing so now they try to keep you occupied";
  room.style.display="none";
- burp.style.display="block";
+burp.style.display="block"
+
 });
 // cheese choice 1 
 eatCheese.addEventListener('mouseenter', function()  {
-text.innerHTML="that cheese was good";
-
+text.innerHTML="When you try to talk to it it grabs you and you wered never found ";
+room.style.display="none";
+eatCheese.style.display="none";
+grabs.style.display="block";
 });
